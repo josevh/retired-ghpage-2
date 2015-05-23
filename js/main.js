0a3dcdf6603e0e3991947fbe6c06038fe98bdc3f
Jose V Herrera <josevictorherrera@gmail.com>
2015-05-23 14:44:02 -0700

    got rid of unused code, add code to have arrow btn toggle

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 /*jslint browser: true*/
 /*global $ */
-                  
+
 $("#menu-toggle").click(function (e) {
+  // toggle show/hide sidebar
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
-    $("#sidebar-wrapper").toggleClass("toggled");
+
+  // sidebar arrow toggle direction
+    if ( $("#menu-toggle-icon").hasClass("glyphicon glyphicon-menu-right") ) {
+      $("#menu-toggle-icon").removeClass("glyphicon glyphicon-menu-right").addClass("glyphicon glyphicon-menu-left");
+    } else {
+      $("#menu-toggle-icon").removeClass("glyphicon glyphicon-menu-left").addClass("glyphicon glyphicon-menu-right");
+    }
 });
 
 $(document).ready(function(){
-    
+
 });
