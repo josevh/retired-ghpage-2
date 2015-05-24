42ad52a28f1d36d9616daf1c3e2ee4c2ca771591
Jose Herrera Koding <josevictorherrera@gmail.com>
2015-05-24 03:46:01 +0000

    enable avatar toggle to enlarge and minimize, move menu down

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
-/*jslint browser: true*/
-/*global $ */
-
-$("#menu-toggle").click(function (e) {
-  // avatar enlarges
-    $("#avatar").attr('src', 'img/avatar.jpg');
-    $("#avatar").fadeIn('400');
-    $("#avatar").toggleClass("large");
+var main = function(){
     
-  // toggle show/hide sidebar
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-
-  // sidebar arrow toggle direction
-    if ( $("#menu-toggle button i").hasClass("fa-angle-double-right") ) {
-      $("#menu-toggle button i").removeClass("fa-angle-double-right").addClass("fa-angle-double-left");
-    } else {
-      $("#menu-toggle button i").removeClass("fa-angle-double-left").addClass("fa-angle-double-right");
-    }
+    $("#menu-toggle").click(function (e) {
+      // avatar enlarges
+        if ( $("#avatar").attr('src') == 'img/avatar-mini.png') {
+            $("#avatar").attr('src', "img/avatar.jpg");
+        } else {
+            $("#avatar").attr('src', 'img/avatar-mini.png');
+        }
+      // avatar enlarges, sidebar menu pushed down 
+        $(".sidebar-brand").toggleClass("large");
+        $("#avatar").toggleClass("large");
+        
+     
+      // toggle show/hide sidebar
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
     
+     
+      // sidebar arrow toggle direction
+        if ( $("#menu-toggle button i").hasClass("fa-angle-double-right") ) {
+          $("#menu-toggle button i").removeClass("fa-angle-double-right").addClass("fa-angle-double-left");
+        } else {
+          $("#menu-toggle button i").removeClass("fa-angle-double-left").addClass("fa-angle-double-right");
+        }
+        
+    
+    });
+    
+    $(document).ready(function(){
+    
+    });
+}
 
-});
-
-$(document).ready(function(){
-
-});
+$(document).ready(main);
