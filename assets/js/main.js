de9f1f75cff3ed94e74ff62d3544383fc20ae1c4
Jose Herrera <josevictorherrera@gmail.com>
2015-08-30 18:28:58 +0000

    change contact bg image

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
                 $('body').css('background-image', 'url(https://farm5.staticflickr.com/4016/4576077203_af85ec5163_o.jpg)');
                 break;
             case '#contact':
-                $('body').css('background-image', 'url(https://farm4.staticflickr.com/3876/14593623379_39b8cd6034_o.jpg)');
+                $('body').css('background-image', 'url(https://farm3.staticflickr.com/2686/4438624473_2ef3b3a772_o.jpg)');
                 break;
             default:
                 // code
