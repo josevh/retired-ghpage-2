d9b3e5f74ac4c083b4c6e6537ddf0d48857579f2
Jose Herrera <josevictorherrera@gmail.com>
2015-08-30 17:59:32 +0000

    remove unused code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,3 @@
-var main = function(){
+$(document).ready(function(){
     
-    $("#menu-toggle").click(function (e) {
-      // avatar enlarges
-        if ( $("#avatar").attr('src') == 'img/avatar-mini.png') {
-            $("#avatar").attr('src', "img/avatar.jpg");
-        } else {
-            $("#avatar").attr('src', 'img/avatar-mini.png');
-        }
-      // avatar enlarges, sidebar menu pushed down 
-        $(".sidebar-brand").toggleClass("large");
-        $("#avatar").toggleClass("large");
-        
-     
-      // toggle show/hide sidebar
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    
-     
-      // sidebar arrow toggle direction
-        if ( $("#menu-toggle button i").hasClass("fa-angle-double-right") ) {
-          $("#menu-toggle button i").removeClass("fa-angle-double-right").addClass("fa-angle-double-left");
-        } else {
-          $("#menu-toggle button i").removeClass("fa-angle-double-left").addClass("fa-angle-double-right");
-        }
-        
-    
-    });
-    
-    $(document).ready(function(){
-    
-    });
-}
-
-$(document).ready(main);
+});
