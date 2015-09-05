d2a52257f0f908954793210d55614010a054f34c
Jose Herrera <josevictorherrera@gmail.com>
2015-09-05 03:38:38 +0000

    move bg imgs locally

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
     $('#myTab > li').click(function () {
         switch ($(this).children('a').attr('href')) {
             case '#about':
-                $('body').css('background-image', 'url(http://c3.staticflickr.com/3/2934/14578335119_f6d461865f_h.jpg)');
+                $('body').css('background-image', 'url(assets/img/gc1-min.jpg)');
                 break;
             case '#profile':
-                $('body').css('background-image', 'url(https://farm2.staticflickr.com/1099/5144658094_0c398dff61_o.jpg)');
+                $('body').css('background-image', 'url(assets/img/gap1-min.jpg)');
                 break;
             case '#portfolio':
-                $('body').css('background-image', 'url(https://farm5.staticflickr.com/4016/4576077203_af85ec5163_o.jpg)');
+                $('body').css('background-image', 'url(assets/img/flower1-min.jpg)');
                 break;
             case '#contact':
-                $('body').css('background-image', 'url(https://farm3.staticflickr.com/2686/4438624473_2ef3b3a772_o.jpg)');
+                $('body').css('background-image', 'url(assets/img/peacock1-min.jpg)');
                 break;
             default:
                 // code
@@ -27,11 +27,11 @@ $(document).ready(function(){
 
 $(window).load(function() {
       preload([
-    'http://c3.staticflickr.com/3/2934/14578335119_f6d461865f_h.jpg',
-    'https://farm2.staticflickr.com/1099/5144658094_0c398dff61_o.jpg',
-    'https://farm5.staticflickr.com/4016/4576077203_af85ec5163_o.jpg',
-    'https://farm3.staticflickr.com/2686/4438624473_2ef3b3a772_o.jpg'
-]);
+    // 'assets/img/gc1-min.jpg',
+    'assets/img/gap1-min.jpg',
+    'assets/img/flower1-min.jpg',
+    'assets/img/peacock1-min.jpg'
+    ]);
 });
 
 function preload(arrayOfImages) {
