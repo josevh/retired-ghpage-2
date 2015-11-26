e638694b26de99d221e979472e90867b226957ba
Jose V Herrera <josevictorherrera@gmail.com>
2015-11-26 18:21:48 +0000

    disable multiple backgrounds, temp

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,42 +10,42 @@ $(document).ready(function(){
         $(this).tab('show');
     });
     
-    $('#myTab > li').click(function () {
-        if ($( window ).width() >= 768) {
-            switch ($(this).children('a').attr('href')) {
-                case '#about':
-                    $('body').css('background-image', 'url(assets/img/gc1-min.jpg)');
-                    break;
-                case '#profile':
-                    $('body').css('background-image', 'url(assets/img/mac-min.jpg)');
-                    break;
-                case '#portfolio':
-                    $('body').css('background-image', 'url(assets/img/desk-min.jpg)');
-                    break;
-                case '#contact':
-                    $('body').css('background-image', 'url(assets/img/rain-min.jpg)');
-                    break;
-                default:
-                    $('body').css('background-image', 'url(assets/img/gc1-min.jpg)');
-            }    
-        }
+    // $('#myTab > li').click(function () {
+    //     if ($( window ).width() >= 768) {
+    //         switch ($(this).children('a').attr('href')) {
+    //             case '#about':
+    //                 $('body').css('background-image', 'url(assets/img/gc1-min.jpg)');
+    //                 break;
+    //             case '#profile':
+    //                 $('body').css('background-image', 'url(assets/img/mac-min.jpg)');
+    //                 break;
+    //             case '#portfolio':
+    //                 $('body').css('background-image', 'url(assets/img/desk-min.jpg)');
+    //                 break;
+    //             case '#contact':
+    //                 $('body').css('background-image', 'url(assets/img/rain-min.jpg)');
+    //                 break;
+    //             default:
+    //                 $('body').css('background-image', 'url(assets/img/gc1-min.jpg)');
+    //         }    
+    //     }
         
-    });
+    // });
 });
 
-$(window).load(function() {
-      preload([
-    // 'assets/img/gc1-min.jpg',
-    'assets/img/rain-min.jpg',
-    'assets/img/mac-min.jpg',
-    'assets/img/peacock1-min.jpg'
-    ]);
-});
+// $(window).load(function() {
+//       preload([
+//     // 'assets/img/gc1-min.jpg',
+//     'assets/img/rain-min.jpg',
+//     'assets/img/mac-min.jpg',
+//     'assets/img/peacock1-min.jpg'
+//     ]);
+// });
 
-function preload(arrayOfImages) {
-    $(arrayOfImages).each(function(){
-        // $('<img/>')[0].src = this;
-        // Alternatively you could use:
-        (new Image()).src = this;
-    });
-}
+// function preload(arrayOfImages) {
+//     $(arrayOfImages).each(function(){
+//         // $('<img/>')[0].src = this;
+//         // Alternatively you could use:
+//         (new Image()).src = this;
+//     });
+// }
